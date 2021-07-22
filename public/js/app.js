c33cdcd4c433b3a0b1b748e1c8294b9d88f062fb
Sandy <rockerdudesandy@gmail.com>
2021-07-22 09:08:59 +0530

    Changes made for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,7 @@ weatherForm.addEventListener('submit', (event) => {
     msg1.textContent = "loading results..."
     msg2.textContent = ""
     
-    fetch('http://localhost:3000/weather?address=' + address).then((response) => {
+    fetch('/weather?address=' + address).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 msg1.textContent = data.error
